b0b320b48572ee7d8c360ea83a1647dd59bba700
fiorellachilcon <fiorellachilc_20@hotmail.com>
2024-07-24 17:15:16 +1000

    chore: add animations

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,17 +1,22 @@
 import me from '../assets/images/me.svg';
+import SlideInAnimation from '../components/SlideInAnimation';
 
 export default function AboutMe() {
   return (
-    <div id="about" className="lg:min-h-screen flex justify-center items-center flex-row gap-6 sm:gap-14 flex-wrap py-9">
-      <img className="my-9 w-[272px] h-[293px]" src={me} alt="Fiorella"/>
-      <div className="w-full sm:w-1/3 sm:min-w-80 sm:p-0 p-8">
+    <div className="min-h-screen flex justify-center items-center flex-row gap-6 sm:gap-14 flex-wrap py-9" id="about">
+      <img className="w-[272px] h-[293px]" src={me} alt="Fiorella"/>
+      <SlideInAnimation animationType='fade-in-bottom'  styleClasses="w-full sm:w-1/3 sm:min-w-80 sm:p-0 p-8">
         <h2 className="text-2xl mb-9">Hola! I’m Fiorella 👋</h2>
         <p className="text-base text-justify">
-          I'm a front-end developer from Perú, and my journey in tech began with a bootcamp that ignited my passion for programming. Over the past 4 years, I've dedicated myself to creating dynamic and visually appealing user interfaces that showcase my creativity and problem-solving skills. Currently, I'm making my way from the vibrant culture of Peru to the bustling tech scene in Australia, pursuing a Bachelor's degree in IT and Cybersecurity to deepen my knowledge and expertise.
+          I’m a passionate front-end developer from Perú.
+          My tech journey took off with a coding bootcamp, and now I’m exploring the vibrant tech scene in Australia while pursuing a Bachelor’s in IT majoring in Cybersecurity.
+          <br/> <br/>
+          With over 4 years of experience, I’m driven to push the boundaries of design and technology to deliver outstanding results.
+          I thrive on collaborating with teams and embracing new challenges.
           <br/> <br/>
           Feel free to connect with me for opportunities or collaborations!
         </p>
-      </div>
+      </SlideInAnimation>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import me from '../assets/images/me.svg';
+import SlideInAnimation from '../components/SlideInAnimation';
 
 export default function Home() {
   return (
     <div className="w-full h-screen py-9 flex items-center justify-center" id="home">
-      <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl">Fiorella Chilcon</h1>
-          <img className="my-9 w-[272px] h-[293px]" src={me} alt="Fiorella"/>
-          <h2 className="text-2xl">Frontend Developer</h2>
-      </div>
+      <SlideInAnimation animationType='fade-in-bottom' styleClasses="flex flex-col items-center justify-center">
+        <h1 className="text-4xl">Fiorella Chilcon</h1>
+        <img className="my-9 w-[272px] h-[293px]" src={me} alt="Fiorella"/>
+        <h2 className="text-2xl">Frontend Developer</h2>
+      </SlideInAnimation>
     </div>
   )
 }
